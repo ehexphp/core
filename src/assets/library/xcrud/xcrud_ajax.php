c86d16d58ebf4e894e32593123fb7f4e52224a1e
Samson Oyetola <samsoniyanu@hotmail.com>
2024-01-28 23:23:46 -0500

    fix: xcrud

--- src/assets/library/xcrud/xcrud_ajax.php
+++ src/assets/library/xcrud/xcrud_ajax.php
@@ -1,10 +1,11 @@
 <?php
 
-
-
-
 include('xcrud.php');
-// XAMTAX EDIT
-include_once (__DIR__."/../../../Ehex.php");
 header('Content-Type: text/html; charset=' . Xcrud_config::$mbencoding);
+
+// XAMTAX EDIT
+const BASE_PATH = __DIR__."/../../../";
+const PATH_LIB_ASSETS = BASE_PATH;
+include_once (PATH_LIB_ASSETS."Ehex.php");
+
 echo Xcrud::get_requested_instance();
