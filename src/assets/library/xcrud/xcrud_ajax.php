a69b864ba415868d51bf48347a27167bc5385091
Samson Oyetola <samsoniyanu@hotmail.com>
2024-02-03 22:18:03 -0500

    fix: final xcrud fix

--- src/assets/library/xcrud/xcrud_ajax.php
+++ src/assets/library/xcrud/xcrud_ajax.php
@@ -4,15 +4,10 @@ include_once('xcrud.php');
 header('Content-Type: text/html; charset=' . Xcrud_config::$mbencoding);
 
 // XAMTAX EDIT
-//const BASE_PATH = __DIR__."/../../../";
-//const PATH_LIB_ASSETS = BASE_PATH;
-//include_once (PATH_LIB_ASSETS."Ehex.php");
-
-
 @define("BASE_PATH", $_SERVER['DOCUMENT_ROOT']."/");
-@define("PATH_LIB_ASSETS", BASE_PATH."vendor/ehexphp/core/src/");
+@define("PATH_LIB_ASSETS", BASE_PATH."vendor/ehexphp/core/src/assets/");
 
-//die(BASE_PATH."vendor/ehexphp/core/src/");
+$ehexPath = BASE_PATH."vendor/ehexphp/core/src/Ehex.php";
 
-include_once(BASE_PATH."vendor/ehexphp/core/src/".'Ehex.php');
+include_once($ehexPath);
 echo Xcrud::get_requested_instance();
